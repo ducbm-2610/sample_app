a789a9c026402bb19a2562f3b103f637c9e9ccc0
ducbm-2610 <ducbui.4business@gmail.com>
2024-07-08 15:19:39 +0700

    Chapter 6
    Co-authored-by: Duc Bui <ducgrand2001@gmail.com>

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,7 @@ module RailsTutorial
   class Application < Rails::Application
     config.load_defaults 7.0
 
-    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**", "*.{rb,yml}")]
     config.i18n.available_locales = [:en, :vi]
     config.i18n.default_locale = :en
   end
