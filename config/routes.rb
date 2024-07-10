9d912022719e3ac14b819cda1be468b3571d16db
ducbm-2610 <ducbui.4business@gmail.com>
2024-07-10 10:09:46 +0700

    Chapter 7: Sign up
    Co-authored-by: Duc Bui <ducgrand2001@gmail.com>

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
+  root "static_pages#home"
+
   get 'static_pages/home'
   get 'static_pages/help'
   resources :static_pages, only: %i(home help)
+
+  get "/signup", to: "users#new"
+  post "/signup", to: "users#create"
+  resources :users, only: :show
+
 end
