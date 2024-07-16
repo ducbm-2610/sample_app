b4fe76e37afa21c734f0f87445eaff153766e204
ducbm-2610 <ducbui.4business@gmail.com>
2024-07-16 10:05:46 +0700

    Chapter 9: Advanced login
    Co-authored-by: Duc Bui <ducgrand2001@gmail.com>

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,12 @@
 class User < ApplicationRecord
-  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  attr_accessor :remember_token
 
   validates :name, presence: true,
   length: {in: 1..50, too_long: I18n.t("users.warnings.max_length_50"),
            too_short: I18n.t("users.warnings.min_length_1")}
 
   validates :email, presence: true, length: {maximum: Settings.max_255},
-    format: {with: VALID_EMAIL_REGEX}, uniqueness: true
+    format: {with: Regexp.new(Settings.VALID_EMAIL_REGEX)}, uniqueness: true
 
   has_secure_password
 
@@ -28,12 +28,31 @@ class User < ApplicationRecord
     email.downcase!
   end
 
-  def self.digest string
-    cost = if ActiveModel::SecurePassword.min_cost
-             BCrypt::Engine::MIN_COST
-           else
-             BCrypt::Engine.cost
-           end
-    BCrypt::Password.create string, cost:
+  class << self
+    def digest string
+      cost = if ActiveModel::SecurePassword.min_cost
+               BCrypt::Engine::MIN_COST
+             else
+               BCrypt::Engine.cost
+             end
+      BCrypt::Password.create string, cost:
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update_column :remember_digest, nil
   end
 end
